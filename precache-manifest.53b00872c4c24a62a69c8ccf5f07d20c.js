8b8727b0040bbe165cf2807b495c93c7c544a96e
Rafael Arthou <r.arthou@hotmail.com>
2022-08-15 00:59:46 -0300

    Updates

---
+++ precache-manifest.53b00872c4c24a62a69c8ccf5f07d20c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "928ecfa572a1de53da95",
+    "url": "/relais-med/static/css/main.b9d5fe83.chunk.css"
+  },
+  {
+    "revision": "928ecfa572a1de53da95",
+    "url": "/relais-med/static/js/main.0945475f.chunk.js"
+  },
+  {
+    "revision": "938b3d5c5e1ed3279338",
+    "url": "/relais-med/static/js/runtime~main.5a70e5e6.js"
+  },
+  {
+    "revision": "fbc8a83328c7714466f9",
+    "url": "/relais-med/static/css/2.2b764b2c.chunk.css"
+  },
+  {
+    "revision": "fbc8a83328c7714466f9",
+    "url": "/relais-med/static/js/2.fba0dde3.chunk.js"
+  },
+  {
+    "revision": "b72ddc5ecd0bd91a4dbbf8ce892c5698",
+    "url": "/relais-med/static/media/logoRelais.b72ddc5e.png"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/relais-med/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/relais-med/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/relais-med/static/media/primeicons.e01fd413.eot"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/relais-med/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "25fd48e4dd075a43ca4d61a9e4caee65",
+    "url": "/relais-med/index.html"
+  }
+];
